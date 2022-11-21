a1b171c999ddd3bc309dc697fed5b07074a279c5
paarthvisharma <sharma.paa@northeastern.edu>
2022-11-20 22:21:25 -0500

    procedures

--- paarthvi_procedures.sql
+++ paarthvi_procedures.sql
@@ -1,1 +1,20 @@
 USE upi_system;
+DROP PROCEDURE IF EXISTS createBank;
+DELIMITER //
+CREATE PROCEDURE createBank(
+	bank_name VARCHAR(50),
+    bank_reg_id INT,
+    routing_number INT,
+    building_number INT,
+    street_name VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    pin VARCHAR(6))
+BEGIN
+	INSERT INTO bank (bank_name, bank_reg_id, routing_number, building_number, street_name, city, state, pin)
+    VALUES (bank_name, bank_reg_id, routing_number, building_number, street_name, city, state, pin);
+END//
+DELIMITER ;
+SELECT * FROM bank;
+
+CALL createbank("SBI", 1 , 12345 , 12 , "University Road" , "Jammu", "J&K", "180006");
