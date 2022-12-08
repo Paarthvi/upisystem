f3aa202419377c09f2543f0310cd4921c5dc15b8
paarthvisharma <sharma.paa@northeastern.edu>
2022-12-08 02:20:20 -0500

    almost done with the views

--- dbms/website/__init__.py
+++ dbms/website/__init__.py
@@ -3,6 +3,7 @@ from .views import views
 from .auth import auth
 from .bankTransaction import banktransaction
 from .upiTransaction import upiTransaction
+from .userProfile import userprofile
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +12,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix= "/")
     app.register_blueprint(banktransaction, url_prefix= "/")
     app.register_blueprint(upiTransaction, url_prefix= "/")
+    app.register_blueprint(userprofile, url_prefix= "/")
     return app
 
 # if __name__ == 'main':
---
+++ dbms/website/userProfile.py
@@ -0,0 +1,54 @@
+from flask import Blueprint,render_template, request, flash, session, redirect
+from .dbConnection import conn
+
+userprofile = Blueprint('userProfile', __name__)
+cursor = conn.cursor()
+
+@userprofile.route('/userProfile', methods = ['GET', 'POST'])
+def viewUserProfile():
+    if request.method == 'GET':
+        if 'ssn' not in session.keys():
+            flash("Login to a user first", category='error')
+            return redirect('/login')
+        return render_template("userProfile.html",
+                               firstName=session['first_name'],
+                               lastName=session['last_name'],
+                               ssn=session['ssn'],
+                               houseNumber=session['house_number'],
+                               streetName=session['street_name'],
+                               city=session['city'],
+                               state=session['state'],
+                               pin=session['pin']
+                               )
+
+@userprofile.route('/accountDetails', methods = ['GET'])
+def bankDetails():
+    ssn = session['ssn']
+    cursor.execute("SELECT bank_name, branch.branch_id, account_number,"
+                   " branch.building_number, branch.street_name, branch.city, "
+                   "branch.pin FROM bank_account "
+                   "JOIN branch ON  bank_account.branch_id = branch.branch_id "
+                   "JOIN bank ON bank.bank_reg_id = branch.bank_reg_id where ssn =  %s;",(ssn,))
+    all_rows = cursor.fetchall()
+    table = """<table>
+                <tr>
+                    <th>Bank Name</th>
+                    <th>Branch ID</th>
+                    <th>Bank Account</th>
+                    <th>Building Number</th>
+                    <th>Street Name</th>
+                    <th>City</th>
+                    <th>Pin</th>
+                </tr>\n"""
+    for i in all_rows:
+        table += f"""<tr>
+                    <td>{i[0]}</td>
+                    <td>{i[1]}</td>
+                    <td>{i[2]}</td>
+                    <td>{i[3]}</td>
+                    <td>{i[4]}</td>
+                    <td>{i[5]}</td>
+                    <td>{i[6]}</td>
+                </tr>\n"""
+    table += "</table>"
+    return table
