4edb726a7935377653a6304637c54c39dbfc7f01
prajwalshenoy <prajwalkpshenoy@gmail.com>
2022-12-09 16:23:41 -0500

    fixed graphs

--- dbms/website/views.py
+++ dbms/website/views.py
@@ -9,9 +9,8 @@ def home():
     if 'ssn' not in session.keys():
             flash("Login to a user first", category='error')
             return redirect('/login')
-    cursor.execute("SELECT count(*) from bank_transactions where transaction_message "
-                   "= 'Bank to Bank transaction';")
+    cursor.execute("SELECT count(*) from bank_transactions where transaction_message = 'Bank to Bank transaction';")
     bank_output = cursor.fetchall()
     cursor.execute("SELECT count(*) from upi_transaction;")
     upi_output = cursor.fetchall()
-    return render_template("home.html", bank_transactions = bank_output, upi_transactions = upi_output)
+    return render_template("home.html", bank_transactions = bank_output[0][0]/2, upi_transactions = upi_output[0][0])
