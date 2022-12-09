73ecb296af647c8122a0e1073d714beba0f5342a
paarthvisharma <sharma.paa@northeastern.edu>
2022-12-08 21:07:25 -0500

    view transactions updated

--- dbms/website/viewTransaction.py
+++ dbms/website/viewTransaction.py
@@ -16,11 +16,12 @@ def viewDetails():
                                ssn=session['ssn'],)
     if request.method == 'POST':
         accountNumber = request.form.get('bankAccountNumber')
+        emailID = request.form.get('email')
         if len(accountNumber)!=10:
             flash("Account number should be 10 characters", category='error')
         cursor.callproc("viewTransaction", (accountNumber,))
         for result in cursor.stored_results():
-            all_rows=result.fetchall()
+            all_rows = result.fetchall()
         table = """<table>
                 <tr>
                     <th>Bank Transaction ID</th>
@@ -44,6 +45,39 @@ def viewDetails():
         table += "</table>"
         # return table
         return render_template("displayTransactions.html", length=len(all_rows), table_list=all_rows)
+
+@viewtransactions.route('/viewTransaction', methods =['GET','POST'])
+def viewUPItransactions():
+    if request.method == 'POST':
+        emailID = request.form.get('email')
+        cursor.callproc("viewUPITransaction", (emailID,))
+        for result in cursor.stored_results():
+            all_rows = result.fetchall()
+        table = """<table>
+                <tr>
+                    <th>UPI Transaction ID</th>
+                    <th>Balance </th>
+                    <th>Bank Transaction ID</th>
+                    <th>Transaction Type</th>
+                    <th>Personal Account</th>
+                    <th>Sender/Receiver Account</th>
+                    <th>Transaction Date</th>
+                    <th>Transaction Value</th>
+                </tr>\n"""
+        for i in all_rows:
+            table += f"""<tr>
+                    <td>{i[0]}</td>
+                    <td>{i[1]}</td>
+                    <td>{i[2]}</td>
+                    <td>{i[3]}</td>
+                    <td>{i[4]}</td>
+                    <td>{i[5]}</td>
+                    <td>{i[6]}</td>
+                    <td>{i[7]}</td>
+                </tr>\n"""
+        table += "</table>"
+        # return table
+        return render_template("displayUPITransactions.html", length=len(all_rows), table_list=all_rows)
 #
 # @viewtransactions.route('/transactionDetails',methods = ['GET'])
 # def transactionDetails():
